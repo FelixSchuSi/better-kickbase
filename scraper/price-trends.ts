e3deb3daa194689cfc3be6ad81a611a1bdd2c546
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-07-30 15:49:09 +0200

    Trigger pipeline

--- scraper/price-trends.ts
+++ scraper/price-trends.ts
@@ -71,6 +71,7 @@ async function main() {
     date: new Date().toISOString().split('T')[0],
     players: priceTrends
   };
+
   writeFileSync(path.join(__dirname, 'price-trends.json'), JSON.stringify(priceTrendObj));
 }
 
