9470b7d6f740a7faa8e30d003507aa729b46e044
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-07-31 12:42:48 +0200

    Optimize img replace

--- scripts/live-matchday-img-replace.ts
+++ scripts/live-matchday-img-replace.ts
@@ -1,8 +1,15 @@
 import { selectAll } from './helpers/select-all';
 import { Selectors } from './helpers/selectors';
+import { routerService } from './router.service';
+
+let runObserver: boolean = false;
+
+routeListener(routerService.getPath());
+routerService.subscribe(routeListener);
 
 const observer: MutationObserver = new MutationObserver((mutations: MutationRecord[]) => {
-  mutations.some((record: MutationRecord) => {
+  if (!runObserver) return;
+  mutations.forEach((record: MutationRecord) => {
     record.addedNodes.forEach((e: Node) => {
       if (e instanceof HTMLElement && e.classList?.contains('live')) {
         const playerImages: HTMLImageElement[] = <HTMLImageElement[]>(
@@ -16,7 +23,6 @@ const observer: MutationObserver = new MutationObserver((mutations: MutationReco
         }
       }
     });
-    return false;
   });
 });
 
@@ -24,3 +30,16 @@ observer.observe(document, {
   childList: true,
   subtree: true
 });
+
+async function routeListener(path: string) {
+  switch (path) {
+    case 'livematchday': {
+      runObserver = true;
+      break;
+    }
+    default: {
+      runObserver = false;
+      break;
+    }
+  }
+}
